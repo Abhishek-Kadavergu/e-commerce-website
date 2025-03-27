1f72dcf53d33a97fff0722e6b6ae097d2fd443d2
abhishek <abhishekkadavergu944@gmail.com>
2025-03-27 05:39:42 +0530

    the first one

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { NavLink, Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between px-6 py-5 font-medium">
+      <img src={assets.logo} className="w-32" alt="logo image" />
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+          <p>COLLECTIONS</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
+      <div className="flex items-center gap-6">
+        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <div className="group relative">
+          <img
+            className="w-5 cursor-pointer"
+            src={assets.profile_icon}
+            alt=""
+          />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500  rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
+          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
+            3
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Collection = () => {
+  return (
+    <div>
+      <h3>collections</h3>
+    </div>
+  );
+};
+
+export default Collection;
